125eb894f98ea66deecaecd27a8902de2bde127e
藤原拓海 <1321426340@qq.com>
2021-08-09 09:22:41 +0800

    字符串

--- src/leptjson.h
+++ src/leptjson.h
@@ -1,3 +1,5 @@
+#include <stddef.h> /* size_t */
+
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
@@ -12,6 +14,12 @@ typedef enum {
 }lept_type;
 
 typedef struct {
+    union {
+        struct {
+            char* content; size_t length;
+        }string;
+        double number;
+    };
     lept_type type;
 }lept_value;
 
@@ -19,11 +27,30 @@ enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_ROOT_NOT_SINGULAR,
+    LEPT_PARSE_NUMBER_TOO_BIG,
+    LEPT_PARSE_MISS_QUOTATION_MARK,
+    LEPT_PARSE_INVALID_STRING_ESCAPE,
+    LEPT_PARSE_INVALID_STRING_CHAR
 };
 
-int lept_parse(lept_value* v, const char* json);
+#define lept_set_null(value) lept_free(value)
+#define lept_init(v) do { (v)->type = LEPT_NULL; }while(0)
 
-lept_type lept_get_type(const lept_value* v);
+int lept_parse(lept_value* value, const char* json);
+
+lept_type lept_get_type(const lept_value* value);
+
+double lept_get_number(const lept_value* value);
+void lept_set_number(lept_value* value, double n);
+
+int lept_get_boolean(lept_value* value);
+void lept_set_boolean(lept_value* value, int b);
+
+const char* lept_get_string(const lept_value* value);
+size_t lept_get_string_length(const lept_value* value);
+void lept_set_string(lept_value* value, const char* s, size_t len);
+
+void lept_free(lept_value* value);
 
 #endif /* LEPTJSON_H__ */
